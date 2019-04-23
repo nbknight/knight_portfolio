41fe62bf448179a29f827065db28e6995e9204e8
nbknight <nicholas.b.knight@gmail.com>
2019-04-22 23:21:15 -0400

    Built custom action to produce toggle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
-  resources :blogs
+  resources :blogs do
+
+    member do
+      get :toggle_status
+    end
+
+  end
 
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
