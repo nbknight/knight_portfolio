917d965180f4d4278ac7fa38044a92e65da66f1e
nbknight <nicholas.b.knight@gmail.com>
2019-07-21 10:11:24 -0400

    Implemented basic auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
-  resources :blogs do
-
-    member do
-      get :toggle_status
-    end
-
-  end
-
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   
   root to: 'pages#home'
